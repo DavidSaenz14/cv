817afe64b1aceccea4453380ae92c603203b7092
“DavidSaenz” <dam115.2023@gmail.com>
2023-01-18 10:11:44 +0100

    Funcion Suma

--- src/CALCULADORA/CALCULADORA.java
+++ src/CALCULADORA/CALCULADORA.java
@@ -2,10 +2,8 @@ package CALCULADORA;
 
 public class CALCULADORA {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-
-	}
+	public int sumar(int a, int b) {
+		return a + b;
+		}
 
 }
