041bc0d6c3ef8252ace918eb281b53b02d8f9951
“DavidSaenz” <dam115.2023@gmail.com>
2023-01-18 10:07:43 +0100

    Proyecto Base

---
+++ src/CALCULADORA/CALCULADORA.java
@@ -0,0 +1,11 @@
+package CALCULADORA;
+
+public class CALCULADORA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
---
+++ src/CALCULADORA/principal.java
@@ -0,0 +1,10 @@
+package CALCULADORA;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CALCULADORA micalc= new CALCULADORA();
+	}
+
+}
